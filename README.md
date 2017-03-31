09028646037627ff6c2245f211bb0acfab9874a5
= <knoc720@gmail.com>
2017-03-31 15:47:13 +0900

    배포분류 추가

---
+++ README.md
@@ -0,0 +1,1 @@
+java -Dspring.profiles.activce="dev" -jar spring-jpa-example-0.0.1-SNAPSHOT.jar
--- insert_book.sql
+++ insert_book.sql
@@ -1,2 +1,13 @@
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reg_date` datetime NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
 INSERT INTO test_db.book(author, title, description, reg_date) VALUES('kim', 'Who are you', 'novel', NOW());
 INSERT INTO test_db.book(author, title, description, reg_date) VALUES('홍길동', '누구냐 넌?', '소설', NOW());
